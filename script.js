4fa8a6901063b07d29a96794d81459e77ce46747
BastosLaG <bastien.capiaux@gmail.com>
2024-12-23 01:41:23 +0100

    image fullscreen

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-// Fonction pour gérer l'affichage des sections déroulantes
 document.addEventListener('DOMContentLoaded', () => {
-    const sections = document.querySelectorAll('section strong'); // Tous les titres des sections
-
+    const sections = document.querySelectorAll('section strong'); 
     sections.forEach(section => {
         section.addEventListener('click', () => {
-            // Récupérer le div qui suit le titre
             const contentDiv = section.nextElementSibling;
-
-            // Si la div a la classe 'content', on la bascule entre visible et cachée
             if (contentDiv && contentDiv.classList.contains('content')) {
                 contentDiv.classList.toggle('active');
             }
         });
     });
 });
-// Fonction pour ouvrir l'image en plein écran
 function openFullscreen(img) {
     var fullscreenContainer = document.getElementById('fullscreen-container');
     var fullscreenImage = document.getElementById('fullscreen-image');
     
-    fullscreenImage.src = img.src;  // L'image source est mise dans l'image en plein écran
-    fullscreenContainer.style.display = 'flex';  // Afficher le conteneur
+    fullscreenImage.src = img.src;  
+    fullscreenContainer.style.display = 'flex';
 }
-
-// Fonction pour fermer l'image en plein écran
 function closeFullscreen() {
     var fullscreenContainer = document.getElementById('fullscreen-container');
-    fullscreenContainer.style.display = 'none';  // Masquer le conteneur
+    fullscreenContainer.style.display = 'none';
 }
+document.getElementById('fullscreen-container').addEventListener('click', function(event) {
+    if (event.target === this) {
+        closeFullscreen();
+    }
+});
